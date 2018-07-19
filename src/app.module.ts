4b6a785942652f310ea627bcc2e22d5412a4fdcf
Jérôme Précigout <jerome.precigout@smatis.fr>
2018-07-19 09:39:40 +0200

    modification docker-compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { CouponModule } from './coupon/coupon.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/coupon'), CouponModule],
+  imports: [MongooseModule.forRoot('mongodb://mongo/coupon'), CouponModule],
   controllers: [AppController],
   providers: [AppService],
 })
