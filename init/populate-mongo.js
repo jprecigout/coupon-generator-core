b51c022188fc5c17290d9040c8b10b42dce71316
Jérôme Précigout <jerome.precigout@smatis.fr>
2018-07-27 11:18:19 +0200

    nettoyage

--- init/populate-mongo.js
+++
@@ -1,5 +0,0 @@
-curl -d '{"isPercent":false, "amount": 5, "activate": true}' -H "Content-Type: application/json" -X POST http://localhost:3000/coupon/create
-
-curl -X GET "http://localhost:3000/coupon/edit/5b4de5092fa29d5515f2aa1f"
-
-curl -X GET "http://localhost:3000/coupon/all"
